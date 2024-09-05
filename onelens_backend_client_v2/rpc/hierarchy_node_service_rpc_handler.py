1357062033dd66c0092ef301ecd462e888815d5c
parteek65 <parteek@astuto.ai>
2024-09-05 16:11:43 +0530

    added recommendation param change from actul to current

--- onelens_backend_client_v2/rpc/hierarchy_node_service_rpc_handler.py
+++ onelens_backend_client_v2/rpc/hierarchy_node_service_rpc_handler.py
@@ -44,10 +44,19 @@ from onelens_backend_client_v2.models import GetHierarchyFlatResponse
 from onelens_backend_client_v2.models import GetHierarchyNodeByIdRequest
 
 
+from onelens_backend_client_v2.models import GetHierarchyNodeByIdResponse
+
+
 from onelens_backend_client_v2.models import GetLeafNodesRequest
 
 
 from onelens_backend_client_v2.models import GetLeafNodesResponse
+
+
+from onelens_backend_client_v2.models import GetMetricsCountRequest
+
+
+from onelens_backend_client_v2.models import HierarchyNodeResourceMetrics
 
 
 from onelens_backend_client_v2.models import PublishCustomHierarchyRequest
@@ -60,6 +69,12 @@ from onelens_backend_client_v2.models import UpdateHierarchyNodeRequest
 
 
 from onelens_backend_client_v2.models import UpdateHierarchyNodeResponse
+
+
+from onelens_backend_client_v2.models import ValidateHierarchyNodeFieldsRequest
+
+
+from onelens_backend_client_v2.models import ValidateHierarchyNodeFieldsResponse
 
 
 from onelens_backend_client_v2.api_client import ApiClient, RequestSerialized
@@ -814,7 +829,7 @@ class HierarchyNodeServiceRpcHandler:
         _content_type: Optional[StrictStr] = None,
         _headers: Optional[Dict[StrictStr, Any]] = None,
         _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
-    ) -> Any:
+    ) -> GetHierarchyNodeByIdResponse:
         """get hierarchy node by id
 
 
@@ -852,7 +867,7 @@ class HierarchyNodeServiceRpcHandler:
         )
 
         _response_types_map: Dict[str, Optional[str]] = {
-            "200": "Any",
+            "200": "GetHierarchyNodeByIdResponse",
             "422": "HTTPValidationError",
         }
         response_data = self.api_client.call_api(
@@ -1029,6 +1044,127 @@ class HierarchyNodeServiceRpcHandler:
         return self.api_client.param_serialize(
             method="POST",
             resource_path="/rpc/hierarchy_node_service/get_leaf_nodes",
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth,
+        )
+
+    @validate_call
+    def get_metrics_count(
+        self,
+        request: GetMetricsCountRequest,
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)], Annotated[StrictFloat, Field(gt=0)]
+            ],
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> HierarchyNodeResourceMetrics:
+        """get metrics count
+
+
+
+        :param request: (required)
+        :type request: GetMetricsCountRequest
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """
+
+        _param = self._get_metrics_count_serialize(
+            request=request,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index,
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            "200": "HierarchyNodeResourceMetrics",
+            "422": "HTTPValidationError",
+        }
+        response_data = self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+    def _get_metrics_count_serialize(
+        self,
+        request: GetMetricsCountRequest,
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> RequestSerialized:
+        _host = None
+
+        _collection_formats: Dict[str, str] = {}
+
+        _path_params: Dict[str, str] = {}
+        _query_params: List[Tuple[str, str]] = []
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[str, Union[str, bytes]] = {}
+        _body_params: Optional[bytes] = None
+
+        # process the body parameter
+        if request is not None:
+            _body_params = request
+
+        # set the HTTP header `Accept`
+        _header_params["Accept"] = self.api_client.select_header_accept(
+            ["application/json"]
+        )
+
+        # set the HTTP header `Content-Type`
+        if _content_type:
+            _header_params["Content-Type"] = _content_type
+        else:
+            _default_content_type = self.api_client.select_header_content_type(
+                ["application/json"]
+            )
+            if _default_content_type is not None:
+                _header_params["Content-Type"] = _default_content_type
+
+        # authentication setting
+        _auth_settings: List[str] = []
+
+        return self.api_client.param_serialize(
+            method="POST",
+            resource_path="/rpc/hierarchy_node_service/get_metrics_count",
             path_params=_path_params,
             query_params=_query_params,
             header_params=_header_params,
@@ -1271,6 +1407,127 @@ class HierarchyNodeServiceRpcHandler:
         return self.api_client.param_serialize(
             method="POST",
             resource_path="/rpc/hierarchy_node_service/update_node",
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth,
+        )
+
+    @validate_call
+    def validate_hierarchy_node_fields(
+        self,
+        request: ValidateHierarchyNodeFieldsRequest,
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)], Annotated[StrictFloat, Field(gt=0)]
+            ],
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> ValidateHierarchyNodeFieldsResponse:
+        """validate hierarchy node fields
+
+
+
+        :param request: (required)
+        :type request: ValidateHierarchyNodeFieldsRequest
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """
+
+        _param = self._validate_hierarchy_node_fields_serialize(
+            request=request,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index,
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            "200": "ValidateHierarchyNodeFieldsResponse",
+            "422": "HTTPValidationError",
+        }
+        response_data = self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+    def _validate_hierarchy_node_fields_serialize(
+        self,
+        request: ValidateHierarchyNodeFieldsRequest,
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> RequestSerialized:
+        _host = None
+
+        _collection_formats: Dict[str, str] = {}
+
+        _path_params: Dict[str, str] = {}
+        _query_params: List[Tuple[str, str]] = []
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[str, Union[str, bytes]] = {}
+        _body_params: Optional[bytes] = None
+
+        # process the body parameter
+        if request is not None:
+            _body_params = request
+
+        # set the HTTP header `Accept`
+        _header_params["Accept"] = self.api_client.select_header_accept(
+            ["application/json"]
+        )
+
+        # set the HTTP header `Content-Type`
+        if _content_type:
+            _header_params["Content-Type"] = _content_type
+        else:
+            _default_content_type = self.api_client.select_header_content_type(
+                ["application/json"]
+            )
+            if _default_content_type is not None:
+                _header_params["Content-Type"] = _default_content_type
+
+        # authentication setting
+        _auth_settings: List[str] = []
+
+        return self.api_client.param_serialize(
+            method="POST",
+            resource_path="/rpc/hierarchy_node_service/validate_hierarchy_node_fields",
             path_params=_path_params,
             query_params=_query_params,
             header_params=_header_params,
